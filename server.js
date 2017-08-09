b58baf926c1797dff861b91780d1eba8147293a1
keerthigasakthivel <30768481+keerthigasakthivel@users.noreply.github.com>
2017-08-09 16:50:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -79,6 +79,13 @@ return htmlTemplate;
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+var counter=0;
+
+app.get('/counter', function (req, res) {
+    counter=counter+1;
+  res.send(covert.toString());
+});
+
 app.get('/:articleName', function (req, res) {
     var articleName=req.params.articleName;
   res.send(createTemplate(art[articleName]));
